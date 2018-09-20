b0a48319a121f8ea05bd4130a9fd11dcecece9b9
DexterDave97 <debayudhrarhi@gmail.com>
2018-09-21 01:55:07 +0530

    cutscene experiment1

---
+++ ngdc/Assets/Scripts/General/Cutscene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+public class Cutscene : MonoBehaviour {
+    public List<Sprite> cutscenes = new List<Sprite>();
+    public static Image panel;
+    public static Cutscene instance;
+	void Start () {
+        DontDestroyOnLoad(this);
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+            Destroy(this.gameObject);
+
+        for(int i=0;i<cutscenes.Count; i++)
+        {
+            cutscenes[i] = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Sprites/Scenes/Ch1-" + (i + 1) + ".png", typeof(Sprite));
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+	}
+}
