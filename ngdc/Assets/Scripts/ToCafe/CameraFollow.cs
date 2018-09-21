3ca985b840b9dc02622816d5fb12566611883f74
DexterDave97 <debayudhrarhi@gmail.com>
2018-09-21 06:17:50 +0530

    To cafe scene

---
+++ ngdc/Assets/Scripts/ToCafe/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField] float camXPosMax, camXPosMin;
+    [SerializeField] GameObject player; // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        CamFollow();
+	}
+
+    void CamFollow()
+    {
+        if (player.transform.position.x <= camXPosMax && player.transform.position.x >= camXPosMin)
+        {
+            transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        }
+    }
+}
