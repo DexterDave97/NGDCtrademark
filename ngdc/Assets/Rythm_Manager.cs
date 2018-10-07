da82d3a9347a97263b886865b71875b89bed46c7
SKayJaySK <SKayJaySK@users.noreply.github.com>
2018-10-08 00:10:01 +0530

    Rythm

---
+++ ngdc/Assets/Rythm_Manager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rythm_Manager : MonoBehaviour {
+
+    [SerializeField]
+    GameObject W, A, D;
+
+    public static bool Whit, Ahit, Dhit;
+
+    private void Start()
+    {
+        Whit = false;
+        Ahit = false;
+        Dhit = false;
+    }
+
+    private void Update()
+    {
+        if (Whit)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+
+            }
+        }
+
+        if (Ahit)
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+
+            }
+        }
+
+        if (Dhit)
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+
+            }
+        }
+    }
+}
